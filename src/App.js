7556e0b1a10be6666ecd02736c24edb555f69408
Abdul Moiz Rashid <70377003+abdulmoiz-bit@users.noreply.github.com>
2022-04-21 21:28:24 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import News from './components/News';
+import Navbar from './components/Navbar';
+import {
+ 
+  Routes,
+  Route,
+
+} from "react-router-dom";  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<News key="general" pageSize={70} country="us" category="general" />} />
+          <Route exact path="/business" element={ <News key="business" pageSize={70} country="us" category="business" />} />
+          <Route exact path="/entertainment" element={ <News key="entertainment" pageSize={70} country="us" category="entertainment" />} />
+          <Route exact path="/general" element={ <News key="general" pageSize={70} country="us" category="general" />} />
+          <Route exact path="/health" element={ <News key="health" pageSize={70} country="us" category="health" />} />
+          <Route exact path="/science" element={ <News key="science" pageSize={70} country="us" category="science" />} />
+          <Route exact path="/sports" element={ <News key="sports" pageSize={70} country="us" category="sports" />} />
+          <Route exact path="/technology" element={ <News key="technology" pageSize={70} country="us" category="technology" />} />
+        </Routes>
+      
+    </>
   );
 }
 
